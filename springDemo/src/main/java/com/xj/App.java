725278324be4edbf4641dbc6983a95f48690e4d1
George <georgetlgeo@gmail.com>
2023-12-28 11:20:11 +0800

    Modify App.java

--- springDemo/src/main/java/com/xj/App.java
+++ springDemo/src/main/java/com/xj/App.java
@@ -8,6 +8,9 @@ public class App {
         //1.创建Spring IOC 容器
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
         //2.从IOC容器中获取bean实例
+
+
+
         MavenAndSpring mavenAndSpring = (MavenAndSpring) ac.getBean("mavenAndSpring");
         mavenAndSpring.hello();
     }
